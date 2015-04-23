db431876d980efcd0709a2c51dd51a457e90fa2e
h4xxel <isaksson.axel@telia.com>
2015-04-23 21:30:02 +0200

    draw the temperature on the display

---
+++ main.c
@@ -0,0 +1,409 @@
+#include <proc/p32mx320f128h.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define DISPLAY_VDD PORTFbits.RF6
+#define DISPLAY_VBATT PORTFbits.RF5
+#define DISPLAY_COMMAND_DATA PORTFbits.RF4
+#define DISPLAY_RESET PORTGbits.RG9
+
+#define TEMP_SENSOR_ADDR 0x48
+
+typedef enum TempSensorReg TempSensorReg;
+enum TempSensorReg {
+	TEMP_SENSOR_REG_TEMP,
+	TEMP_SENSOR_REG_CONF,
+	TEMP_SENSOR_REG_HYST,
+	TEMP_SENSOR_REG_LIMIT,
+};
+
+char textbuffer[4][16];
+
+static const uint8_t const font[] = {
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 2, 5, 2, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 94, 0, 0, 0, 0,
+	0, 0, 4, 3, 4, 3, 0, 0,
+	0, 36, 126, 36, 36, 126, 36, 0,
+	0, 36, 74, 255, 82, 36, 0, 0,
+	0, 70, 38, 16, 8, 100, 98, 0,
+	0, 52, 74, 74, 52, 32, 80, 0,
+	0, 0, 0, 4, 3, 0, 0, 0,
+	0, 0, 0, 126, 129, 0, 0, 0,
+	0, 0, 0, 129, 126, 0, 0, 0,
+	0, 42, 28, 62, 28, 42, 0, 0,
+	0, 8, 8, 62, 8, 8, 0, 0,
+	0, 0, 0, 128, 96, 0, 0, 0,
+	0, 8, 8, 8, 8, 8, 0, 0,
+	0, 0, 0, 0, 96, 0, 0, 0,
+	0, 64, 32, 16, 8, 4, 2, 0,
+	0, 62, 65, 73, 65, 62, 0, 0,
+	0, 0, 66, 127, 64, 0, 0, 0,
+	0, 0, 98, 81, 73, 70, 0, 0,
+	0, 0, 34, 73, 73, 54, 0, 0,
+	0, 0, 14, 8, 127, 8, 0, 0,
+	0, 0, 35, 69, 69, 57, 0, 0,
+	0, 0, 62, 73, 73, 50, 0, 0,
+	0, 0, 1, 97, 25, 7, 0, 0,
+	0, 0, 54, 73, 73, 54, 0, 0,
+	0, 0, 6, 9, 9, 126, 0, 0,
+	0, 0, 0, 102, 0, 0, 0, 0,
+	0, 0, 128, 102, 0, 0, 0, 0,
+	0, 0, 8, 20, 34, 65, 0, 0,
+	0, 0, 20, 20, 20, 20, 0, 0,
+	0, 0, 65, 34, 20, 8, 0, 0,
+	0, 2, 1, 81, 9, 6, 0, 0,
+	0, 28, 34, 89, 89, 82, 12, 0,
+	0, 0, 126, 9, 9, 126, 0, 0,
+	0, 0, 127, 73, 73, 54, 0, 0,
+	0, 0, 62, 65, 65, 34, 0, 0,
+	0, 0, 127, 65, 65, 62, 0, 0,
+	0, 0, 127, 73, 73, 65, 0, 0,
+	0, 0, 127, 9, 9, 1, 0, 0,
+	0, 0, 62, 65, 81, 50, 0, 0,
+	0, 0, 127, 8, 8, 127, 0, 0,
+	0, 0, 65, 127, 65, 0, 0, 0,
+	0, 0, 32, 64, 64, 63, 0, 0,
+	0, 0, 127, 8, 20, 99, 0, 0,
+	0, 0, 127, 64, 64, 64, 0, 0,
+	0, 127, 2, 4, 2, 127, 0, 0,
+	0, 127, 6, 8, 48, 127, 0, 0,
+	0, 0, 62, 65, 65, 62, 0, 0,
+	0, 0, 127, 9, 9, 6, 0, 0,
+	0, 0, 62, 65, 97, 126, 64, 0,
+	0, 0, 127, 9, 9, 118, 0, 0,
+	0, 0, 38, 73, 73, 50, 0, 0,
+	0, 1, 1, 127, 1, 1, 0, 0,
+	0, 0, 63, 64, 64, 63, 0, 0,
+	0, 31, 32, 64, 32, 31, 0, 0,
+	0, 63, 64, 48, 64, 63, 0, 0,
+	0, 0, 119, 8, 8, 119, 0, 0,
+	0, 3, 4, 120, 4, 3, 0, 0,
+	0, 0, 113, 73, 73, 71, 0, 0,
+	0, 0, 127, 65, 65, 0, 0, 0,
+	0, 2, 4, 8, 16, 32, 64, 0,
+	0, 0, 0, 65, 65, 127, 0, 0,
+	0, 4, 2, 1, 2, 4, 0, 0,
+	0, 64, 64, 64, 64, 64, 64, 0,
+	0, 0, 1, 2, 4, 0, 0, 0,
+	0, 0, 48, 72, 40, 120, 0, 0,
+	0, 0, 127, 72, 72, 48, 0, 0,
+	0, 0, 48, 72, 72, 0, 0, 0,
+	0, 0, 48, 72, 72, 127, 0, 0,
+	0, 0, 48, 88, 88, 16, 0, 0,
+	0, 0, 126, 9, 1, 2, 0, 0,
+	0, 0, 80, 152, 152, 112, 0, 0,
+	0, 0, 127, 8, 8, 112, 0, 0,
+	0, 0, 0, 122, 0, 0, 0, 0,
+	0, 0, 64, 128, 128, 122, 0, 0,
+	0, 0, 127, 16, 40, 72, 0, 0,
+	0, 0, 0, 127, 0, 0, 0, 0,
+	0, 120, 8, 16, 8, 112, 0, 0,
+	0, 0, 120, 8, 8, 112, 0, 0,
+	0, 0, 48, 72, 72, 48, 0, 0,
+	0, 0, 248, 40, 40, 16, 0, 0,
+	0, 0, 16, 40, 40, 248, 0, 0,
+	0, 0, 112, 8, 8, 16, 0, 0,
+	0, 0, 72, 84, 84, 36, 0, 0,
+	0, 0, 8, 60, 72, 32, 0, 0,
+	0, 0, 56, 64, 32, 120, 0, 0,
+	0, 0, 56, 64, 56, 0, 0, 0,
+	0, 56, 64, 32, 64, 56, 0, 0,
+	0, 0, 72, 48, 48, 72, 0, 0,
+	0, 0, 24, 160, 160, 120, 0, 0,
+	0, 0, 100, 84, 84, 76, 0, 0,
+	0, 0, 8, 28, 34, 65, 0, 0,
+	0, 0, 0, 126, 0, 0, 0, 0,
+	0, 0, 65, 34, 28, 8, 0, 0,
+	0, 0, 4, 2, 4, 2, 0, 0,
+	0, 120, 68, 66, 68, 120, 0, 0,
+};
+
+void delay(int cyc) {
+	int i;
+	for(i = cyc; i > 0; i--);
+}
+
+uint8_t spi_send_recv(uint8_t data) {
+	while(!SPI2STATbits.SPITBE);
+	SPI2BUF = data;
+	while(!SPI2STATbits.SPIRBF);
+	return SPI2BUF;
+}
+
+void display_init() {
+	DISPLAY_COMMAND_DATA = 0x0;
+	delay(10);
+	DISPLAY_VDD = 0x0;
+	delay(1000000);
+	
+	spi_send_recv(0xAE);
+	DISPLAY_RESET = 0x0;
+	delay(10);
+	DISPLAY_RESET = 0x1;
+	delay(10);
+	
+	spi_send_recv(0x8D);
+	spi_send_recv(0x14);
+	
+	spi_send_recv(0xD9);
+	spi_send_recv(0xF1);
+	
+	DISPLAY_VBATT = 0x0;
+	delay(10000000);
+	
+	spi_send_recv(0xA1);
+	spi_send_recv(0xC8);
+	
+	spi_send_recv(0xDA);
+	spi_send_recv(0x20);
+	
+	spi_send_recv(0xAF);
+}
+
+void display_string(int line, char *s) {
+	int i;
+	if(line < 0 || line >= 4)
+		return;
+	if(!s)
+		return;
+	
+	for(i = 0; i < 16; i++)
+		if(*s) {
+			textbuffer[line][i] = *s;
+			s++;
+		} else
+			textbuffer[line][i] = ' ';
+}
+
+void display_update() {
+	int i, j, k;
+	int c;
+	for(i = 0; i < 4; i++) {
+		DISPLAY_COMMAND_DATA = 0x0;
+		spi_send_recv(0x22);
+		spi_send_recv(i);
+		
+		spi_send_recv(0x0);
+		spi_send_recv(0x10);
+		
+		DISPLAY_COMMAND_DATA = 0x1;
+		
+		for(j = 0; j < 16; j++) {
+			c = textbuffer[i][j];
+			if(c & 0x80)
+				continue;
+			
+			for(k = 0; k < 8; k++)
+				spi_send_recv(font[c*8 + k]);
+		}
+	}
+}
+
+void i2c_idle() {
+	while(I2C1CON & 0x1F || I2C1STATbits.TRSTAT);
+}
+
+bool i2c_send(uint8_t data) {
+	i2c_idle();
+	I2C1TRN = data;
+	i2c_idle();
+	return !I2C1STATbits.ACKSTAT;
+}
+
+uint8_t i2c_recv() {
+	i2c_idle();
+	I2C1CONbits.RCEN = 1;
+	i2c_idle();
+	I2C1STATbits.I2COV = 0;
+	return I2C1RCV;
+}
+
+void i2c_ack() {
+	i2c_idle();
+	I2C1CONbits.ACKDT = 0;
+	I2C1CONbits.ACKEN = 1;
+}
+
+void i2c_nack() {
+	i2c_idle();
+	I2C1CONbits.ACKDT = 1;
+	I2C1CONbits.ACKEN = 1;
+}
+
+void i2c_start() {
+	i2c_idle();
+	I2C1CONbits.SEN = 1;
+	i2c_idle();
+}
+
+void i2c_restart() {
+	i2c_idle();
+	I2C1CONbits.RSEN = 1;
+	i2c_idle();
+}
+
+void i2c_stop() {
+	i2c_idle();
+	I2C1CONbits.PEN = 1;
+	i2c_idle();
+}
+
+char *fixed_to_string(uint16_t num, char *buf) {
+	bool neg = false;
+	uint32_t n;
+	char *tmp;
+	
+	if(num & 0x8000) {
+		num = ~num + 1;
+		neg = true;
+	}
+	
+	buf += 4;
+	n = num >> 8;
+	tmp = buf;
+	do {
+		*--tmp = (n  % 10) + '0';
+		n /= 10;
+	} while(n);
+	if(neg)
+		*--tmp = '-';
+	
+	n = num;
+	if(!(n & 0xFF)) {
+		*buf = 0;
+		return tmp;
+	}
+	*buf++ = '.';
+	while((n &= 0xFF)) {
+		n *= 10;
+		*buf++ = (n >> 8) + '0';
+	}
+	*buf = 0;
+	
+	return tmp;
+}
+
+uint32_t strlen(char *str) {
+	uint32_t n = 0;
+	while(*str++)
+		n++;
+	return n;
+}
+
+int main(void) {
+	uint16_t temp;
+	char buf[32], *s, *t;
+	
+	/* Set up peripheral bus clock */
+	OSCCONbits.PBDIV = 1;
+	
+	/* Set up output pins */
+	AD1PCFG = 0xFFFF;
+	ODCE = 0x0;
+	TRISECLR = 0xFF;
+	PORTE = 0x0;
+	
+	/* Output pins for display signals */
+	PORTF = 0xFFFF;
+	PORTG = (1 << 9);
+	ODCF = 0x0;
+	ODCG = 0xC;
+	TRISFCLR = 0x70;
+	TRISGCLR = 0x200;
+	
+	/* Set up input pins */
+	TRISDSET = (1 << 8);
+	TRISFSET = (1 << 1);
+	
+	/* Set up SPI as master */
+	SPI2CON = 0x0;
+	SPI2BRG = 4;
+	SPI2STATbits.SPIROV = 0;
+	SPI2CONbits.CKP = 1;
+	SPI2CONbits.MSTEN = 1;
+	SPI2CONbits.ON = 1;
+	
+	/* Set up i2c */
+	I2C1CON = 0x0;
+	I2C1CONbits.ON = 0;
+	I2C1BRG = 0x0C2;
+	I2C1STAT = 0x0;
+	I2C1CONbits.SIDL = 1;
+	I2C1CONbits.ON = 1;
+	temp = I2C1RCV;
+	
+	display_init();
+	display_string(0, "Temperature:");
+	display_string(1, "");
+	display_string(2, "");
+	display_string(3, "");
+	display_update();
+	
+	do {
+		i2c_start();
+	} while(!i2c_send(TEMP_SENSOR_ADDR << 1));
+	i2c_send(TEMP_SENSOR_REG_CONF);
+	i2c_send(0x0);
+	i2c_stop();
+	
+	for(;;) {
+		do {
+			i2c_start();
+		} while(!i2c_send(TEMP_SENSOR_ADDR << 1));
+		i2c_send(TEMP_SENSOR_REG_TEMP);
+		do {
+			i2c_start();
+		} while(!i2c_send((TEMP_SENSOR_ADDR << 1) | 1));
+		temp = i2c_recv() << 8;
+		i2c_ack();
+		temp |= i2c_recv();
+		i2c_nack();
+		i2c_stop();
+		
+		s = fixed_to_string(temp, buf);
+		t = s + strlen(s);
+		*t++ = ' ';
+		*t++ = 7;
+		*t++ = 'C';
+		*t++ = 0;
+		
+		display_string(1, s);
+		display_update();
+		delay(1000000);
+	}
+	
+	return 0;
+}
+
